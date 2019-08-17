18f0a7df322e7f137e42056a224619674d270a95
Yeniel Diaz <yenie@LAPTOP-KARDRO8G>
2019-08-17 17:47:41 -0400

    Added enemy and hazards
    Added collision functionality
    Added enemy
    Added hazards
    Added Jumping
    Added Animations and smart cameras

---
+++ TileVania/Assets/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    bool isFacingRight;
+
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0);
+        ChangeDirection();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("something happened");
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        
+    }
+
+    private void ChangeDirection()
+    {
+        if (Mathf.Sign(transform.localScale.x) > 0) isFacingRight = true;
+        else isFacingRight = false;
+
+        if(isFacingRight)
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, myRigidbody.velocity.y);
+        else
+            myRigidbody.velocity = new Vector2(-myRigidbody.velocity.x, myRigidbody.velocity.y);
+    }
+
+}
--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -8,6 +9,8 @@ public class Player : MonoBehaviour
     //config
     [SerializeField] float moveSpeed = 5;
     [SerializeField] float jumpSpeed = 5;
+    [SerializeField] float climbSpeed = 3;
+    [SerializeField] Vector2 deathKick = new Vector2(0, 25);
 
     //state
     bool isAlive = true;
@@ -17,22 +20,29 @@ public class Player : MonoBehaviour
     Rigidbody2D myRigidbody;
     Animator myAnimator;
     Collider2D myCollider;
+    Collider2D myFeetCollider; //collider for the player's feet
+    float startingGravityScale;
 
-    // Start is called before the first frame update
     void Start()
     {
+        //initiate cached references
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider = GetComponent<Collider2D>();
+        myCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        startingGravityScale = myRigidbody.gravityScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        canJump = myCollider.IsTouchingLayers(LayerMask.GetMask("ground"));
+        //call all player methods
+        if (!isAlive) { return;}
         Jump();
         Run();
+        ClimbLadder();
         FlipSprite();
+        ExecuteDeath();
+
     }
     
     private void Run()
@@ -44,16 +54,47 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
+        canJump = myFeetCollider.IsTouchingLayers(LayerMask.GetMask("ground"));
         if (CrossPlatformInputManager.GetButtonDown("Jump") && canJump)
         {
             Vector2 playerVelocity = new Vector2(myRigidbody.velocity.x, jumpSpeed);
             myRigidbody.velocity = playerVelocity;
+
         }
     }
 
+    private void ClimbLadder()
+    {
+        bool isOnStairs = myCollider.IsTouchingLayers(LayerMask.GetMask("ladders"));
+
+        if (isOnStairs)
+        {
+            myAnimator.SetBool("isClimbing", true);
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector2 playerVelocity = new Vector2(myRigidbody.velocity.x, controlThrow * climbSpeed);
+            myRigidbody.velocity = playerVelocity;
+            myRigidbody.gravityScale = 0;
+        }
+        else
+        {
+            myAnimator.SetBool("isClimbing", false);
+            myRigidbody.gravityScale = startingGravityScale;
+        }
+        /*
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > 0; //is true when player is moving vertically
+        if (playerHasVerticalSpeed)
+        {
+            myAnimator.SetBool("isClimbing", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isClimbing", false);
+        }
+        */
+    }
     private void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > 0; //is true when player is moving
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > 0; //is true when player is moving horizontally
         if (playerHasHorizontalSpeed)
         {
             myAnimator.SetBool("isRunning", true);
@@ -62,6 +103,16 @@ public class Player : MonoBehaviour
         else
         {
             myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    private void ExecuteDeath()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("enemy","hazards")))
+        {
+            myAnimator.SetTrigger("isDying");
+            myRigidbody.velocity = deathKick;
+            isAlive = false;
         }
     }
 }
