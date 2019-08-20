0b5ad86accecd48d25523a46cfc710a31024acef
Yeniel Diaz <yenie@LAPTOP-KARDRO8G>
2019-08-19 22:31:39 -0400

    Level_Transitions
    Added Main Menu Level,
    Added Transitional Buttons
    Added Success Level
    Added Level 1
    Added Level 2

---
+++ TileVania/Assets/LevelExit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] int delay = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextSceneAndDelay());
+    }
+
+    private IEnumerator LoadNextSceneAndDelay()
+    {
+        int currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(currSceneIndex + 1);
+    }
+}
+
